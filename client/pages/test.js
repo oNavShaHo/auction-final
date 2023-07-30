5b70e847ae432f7d06f57f4b73f04a5972e70b57
Aryan <aryan602.as@gmail.com>
2023-07-30 16:42:37 +0530

    updated login component to include login and signup logic

--- client/pages/test.js
+++ client/pages/test.js
@@ -1,38 +1,25 @@
-import React, { useState } from 'react'
-import Navbar from "@/components/Navbar";
-import Card from "@/components/Card";
-import Hero from "@/components/Hero";
-import ReactModal from 'react-modal';
-import Image from 'next/image';
-import Login from '@/components/Login';
+import { useState } from 'react';
+import Navbar from '@/components/Navbar';
+import Link from 'next/link';
 
-export default function test() {
+
+export default function AddProduct() {
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [productImage, setProductImage] = useState('');
+  // Add more states for other product attributes as needed
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform the necessary actions to add the product
+    // For example, you can make an API call to store the product data on the server
+    // Don't forget to add validation and error handling
+  };
+
   return (
-    
-    <div>
-      <Navbar/>
-     
-      <Hero/>
-        <div className="mx-20">
+    <div className="">
         
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-      </div>
-   </div>
-  )
+    </div>
+  );
 }
